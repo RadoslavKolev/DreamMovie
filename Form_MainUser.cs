b86f0eb529aa7fd40e24a396808003fefbe3f959
radokolev99 <radoslav.kolev@abv.bg>
2021-05-21 18:28:27 +0300

    Add project files.

---
+++ Form_MainUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DreamMovie
+{
+    public partial class Form_MainUser : Form
+    {
+        public Form_MainUser()
+        {
+            InitializeComponent();
+        }
+
+        private void button_MoviesClick(object sender, EventArgs e)
+        {
+            Form_Movies movie = new Form_Movies();
+            movie.Show();
+            this.Hide();
+        }
+
+        private void button_SearchClick(object sender, EventArgs e)
+        {
+            Form_Search search = new Form_Search();
+            search.Show();
+            this.Hide();
+        }
+
+        private void button_BackToLoginClick(object sender, EventArgs e)
+        {
+            Form_Login lf = new Form_Login();
+            lf.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Form_Terms.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace DreamMovie
+{
+    partial class Form_Terms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(60, 61);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(343, 283);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(122, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Terms of Use";
+            // 
+            // Form_Terms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 364);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form_Terms";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Terms of Service";
+            this.Load += new System.EventHandler(this.Form_Terms_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
