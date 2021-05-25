cb0637e9ee5048b7638e96dfce0074711ddd5f06
radokolev99 <radoslav.kolev@abv.bg>
2021-05-25 12:07:30 +0300

    User Guide added in folder Documentation

--- Form_Terms.Designer.cs
+++ Form_Terms.Designer.cs
@@ -45,11 +45,11 @@ namespace DreamMovie
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(122, 21);
+            this.label1.Location = new System.Drawing.Point(104, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 37);
+            this.label1.Size = new System.Drawing.Size(257, 37);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Terms of Use";
+            this.label1.Text = "Terms of Service";
             // 
             // Form_Terms
             // 
